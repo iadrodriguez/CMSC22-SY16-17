fd3547a3a7b1d866de9a6eb962fad19c597b287c
icelAnnRodriguez <icelannrodriguez@gmail.com>
2016-10-07 15:56:17 +0800

    Update Triangle.java

--- inheritance/Triangle.java
+++ inheritance/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle extends Shape{
 	private double s3;
 
 	public Triangle(double s1, double s2, double s3) {
-		if ((s1 + s2 < s3) || (s1 + s3 < s2) || (s2 + s3 < s1)) {
+		if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
 			throw new IllegalArgumentException("illegal sides of a triangle!");
 		} else {
 			this.s1 = s1;
@@ -18,7 +18,8 @@ public class Triangle extends Shape{
 		return (s1 + s2 + s3);
 	}
 
-	public double getArea() { //sir, dili mi kaimplement sa getArea kay sides ramay parameters, dili base and height.kani amo gihimo kay char char lang para dili muerror kay abstract man gud ang getArea sa superclass. 
-		return 123.22;
+	public double getArea() { 
+		double p = getPerimeter() / 2;
+		return (MATH.sqrt(p));
 	}
 }
