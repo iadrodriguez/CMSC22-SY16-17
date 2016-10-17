fb1b9055f8d139fbea91c2c1645d85206ed7970c
icelAnnRodriguez <icelannrodriguez@gmail.com>
2016-10-18 00:18:12 +0800

    Update Triangle.java

--- inheritance/Triangle.java
+++ inheritance/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle extends Shape{
 	private double s3;
 
 	public Triangle(double s1, double s2, double s3) {
-		if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
+		if (!((s1 + s2 > s3) && (s1 + s3 > s2) && (s2 + s3 > s1))) {
 			throw new IllegalArgumentException("illegal sides of a triangle!");
 		} else {
 			this.s1 = s1;
@@ -16,7 +16,7 @@ public class Triangle extends Shape{
 
 	public Triangle(double s1, double s2, double s3, String color, boolean filled) {
 		super(color, filled);
-		if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
+		if (!((s1 + s2 > s3) && (s1 + s3 > s2) && (s2 + s3 > s1))) {
 			throw new IllegalArgumentException("illegal sides of a triangle!");
 		} else {
 			this.s1 = s1;
